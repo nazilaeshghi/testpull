43a42841e66dfe3115f68e8863bfe2ede74b0a94
nazilaeshghi <nazila.eshghi@gmail.com>
2023-06-14 12:57:02 +0330

    Initial Commit

---
+++ testpull/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testpull
+//
+//  Created by Nazila Eshghi on 3/24/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testpull/testpullApp.swift
@@ -0,0 +1,17 @@
+//
+//  testpullApp.swift
+//  testpull
+//
+//  Created by Nazila Eshghi on 3/24/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct testpullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
